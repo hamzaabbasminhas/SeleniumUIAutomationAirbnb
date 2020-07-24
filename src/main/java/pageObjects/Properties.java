ebc547a4936c00bb2d365d54eafb9c0998ba8615
hamzaabbasminhas <hamza.abbas.minhas@gmail.com>
2020-07-24 12:09:04 +0500

    done

--- src/main/java/pageObjects/Properties.java
+++ src/main/java/pageObjects/Properties.java
@@ -15,14 +15,14 @@ public class Properties {
 			"/html[1]/body[1]/div[4]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/header[1]/div[1]/div[2]/div[1]/button[2]");
 	By guest_filter = By.xpath("//button[contains(text(),'3 guests')]");
 	By num_of_guests = By.xpath(
-			"//body[@class='with-new-header']/div/div/div/div/div[@class='_16grqhk']/main[@id='site-content']/div[@class='content-container']/div[@id='ExploreLayoutController']/div[@class='_e296pg']/div[@class='_8h8epe3']/div[@class='_twmmpk']/div[@class='_19qnt1y']/div[@class='_1gw6tte']/div[@class='_uhpzdny']/div/div/div/div/div/div[@class='_fhph4u']/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]");
+			"//body[@class='with-new-header']/div/div/div/div/div[@class='_16grqhk']/main[@id='site-content']/div[@class='content-container']/div[@id='ExploreLayoutController']/div[@class='_e296pg']/div[@class='_8h8epe3']/div[@class='_twmmpk']/div[@class='_19qnt1y']/div[@class='_1gw6tte']/div[@class='_uhpzdny']/div/div/div/div/div/div[@class='_fhph4u']/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[3]");
+
 	By more_filters_button = By.xpath("//div[@id='menuItemButton-dynamicMoreFilters']//button[@class='_1wc4ltr']");
 	By room_filter_checkBox = By.xpath(
 			"/html[1]/body[1]/div[11]/section[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/button[2]/span[1]");
 	By pool_filter_checkBox = By.xpath("//div[6]//div[1]//div[1]//div[4]//label[1]//span[1]//span[1]");
 	By show_button = By.xpath("//button[@class='_m095vcq']");
-	By property_to_verify = By.xpath(
-			"/html[1]/body[1]/div[4]/div[1]/div[1]/div[1]/div[1]/main[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]");
+	By property_to_verify = By.xpath("//body[@class='with-new-header']/div/div/div/div/div[@class='_16grqhk']/main[@id='site-content']/div[@class='content-container']/div[@id='ExploreLayoutController']/div[@class='_e296pg']/div[@class='_8h8epe3']/div[@class='_twmmpk']/div[@class='_19qnt1y']/div[@class='_1gw6tte']/div[@class='_uhpzdny']/div/div/div/div/div/div[@class='_fhph4u']/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]");
 
 	public Properties(WebDriver driver) {
 		this.driver = driver;
